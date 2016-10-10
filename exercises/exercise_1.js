3f6773289e27d844d09201219d6470a2955a962b
vrunoa <vruno.alassia@gmail.com>
2016-10-10 12:30:34 -0300

    fix exercise

--- exercises/exercise_1.js
+++ exercises/exercise_1.js
@@ -6,7 +6,7 @@ let driver = wd.promiseChainRemote('localhost', 4723);
 // TODO - set the capabilities to test over Chrome in an Android Emulator
 let capabilities = {
   "browserName" : "Chrome",
-  "deviceName" : "KK-GalaxyNexus-portrait-appium",
+  "deviceName" : "Android Emulator",
   "platformName" : "Android",
   "platformVersion" : "4.4.3"
 };
@@ -21,13 +21,13 @@ describe("Test appium workshop excersie 1", _ => {
     await driver.quit();
   });
 
-  it("Test url opens", async (done) => {
+  it("Test url opens on Android Emulator", async (done) => {
     await sleep(5000);
     // TODO - open http://appium-workshop.github.com/ url and verify the title equals Node Conf Appium Aorkshop
-    await driver.get('http://google.com');
+    await driver.get('http://appium-workshop.github.com');
     await sleep(500);
     let title = await driver.title();
-    title.should.equal("Google");
+    title.should.equal("NodeConf Appium Workshop");
     done();
   });
 
