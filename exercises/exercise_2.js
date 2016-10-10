2e934a313c21b8db0c81112e1a769f58a5e12772
vrunoa <vruno.alassia@gmail.com>
2016-10-10 12:30:07 -0300

    add exercise

---
+++ exercises/exercise_2.js
@@ -0,0 +1,34 @@
+import chai from 'chai';
+import wd from 'wd';
+import { sleep } from 'asyncbox';
+
+let driver = wd.promiseChainRemote('localhost', 4723);
+// TODO - set the capabilities to test over Chrome in an Android Emulator
+let capabilities = {
+  "browserName" : "Safari",
+  "deviceName" : "iPhone 5s Simulator",
+  "platformName" : "iOS",
+  "platformVersion" : "9.3"
+};
+
+describe("Test appium workshop excersie 2", _ => {
+
+  before(async () => {
+    await driver.init(capabilities);
+  });
+
+  after(async () => {
+    await driver.quit();
+  });
+
+  it("Test url opens on iOS Simulator", async (done) => {
+    await sleep(5000);
+    // TODO - open http://appium-workshop.github.com/ url and verify the title equals Node Conf Appium Aorkshop
+    await driver.get('http://appium-workshop.github.com');
+    await sleep(500);
+    let title = await driver.title();
+    title.should.equal("NodeConf Appium Workshop");
+    done();
+  });
+
+});
